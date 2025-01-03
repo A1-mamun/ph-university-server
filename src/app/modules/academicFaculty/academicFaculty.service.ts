2086e50041e6ad84e477d6099561a18695b05206
A1-mamun <1154almamun@gmail.com>
2025-01-03 22:24:04 +0600

    Part 3 academic faculty module added

---
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -0,0 +1,34 @@
+import { TAcademicFaculty } from './academicFaculty.interface';
+import { AcademicFaculty } from './academicFaculty.model';
+
+const createAcademicFacultyIntoDB = async (semesterData: TAcademicFaculty) => {
+  const result = await AcademicFaculty.create(semesterData);
+  return result;
+};
+
+const getAllAcademicFacultiesFromDB = async () => {
+  const result = await AcademicFaculty.find();
+  return result;
+};
+
+const getSingleAcademicFacultyFromDB = async (id: string) => {
+  const result = await AcademicFaculty.findById(id);
+  return result;
+};
+
+const updateAcademicFacultyIntoDB = async (
+  id: string,
+  semesterData: Partial<TAcademicFaculty>,
+) => {
+  const result = await AcademicFaculty.findByIdAndUpdate(id, semesterData, {
+    new: true,
+  });
+  return result;
+};
+
+export const AcademicFacultyServices = {
+  createAcademicFacultyIntoDB,
+  getAllAcademicFacultiesFromDB,
+  getSingleAcademicFacultyFromDB,
+  updateAcademicFacultyIntoDB,
+};
